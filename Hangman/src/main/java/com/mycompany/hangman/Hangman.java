b4b75c418f1d3e55e8315369e81858ded8ae4594
Edgar Santana <santanaprogrammer@gmail.com>
2022-07-22 16:50:43 -0400

    started

---
+++ Hangman/src/main/java/com/mycompany/hangman/Hangman.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.hangman;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author <Edgar Santana>
+ */
+public class Hangman {
+
+    public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	String word;
+	    
+	System.out.println("LETS PLAY HANGMAN");
+	System.out.println("Please enter a word to guess");
+	word = input.nextLine();
+    }
+    
+    public String hangman(int wrongCounter){
+        switch(wrongCounter) {
+            case 0:
+                return (" +---+\n" +
+                        "     |\n" +
+                        "     |\n" +
+                        "     |\n" +
+                        "    ===");
+            case 1:
+                return (" +---+\n" +
+                        " O   |\n" +
+                        "     |\n" +
+                        "     |\n" +
+                        "    ===");
+            case 2:
+                return (" +---+\n" +
+                        " O   |\n" +
+                        " |   |\n" +
+                        "     |\n" +
+                        "    ===");
+            case 3:
+                return (" +---+\n" +
+                        " O   |\n" +
+                        " |   |\n" +
+                        " |   |\n" +
+                        "    ===");
+            case 4:
+                return (" +---+\n" +
+                        " O   |\n" +
+                        "-|   |\n" +
+                        " |   |\n" +
+                        "    ===");
+            case 5:
+                return (" +---+\n" +
+                        " O   |\n" +
+                        "-|-  |\n" +
+                        " |   |\n" +
+                        "    ===");
+            case 6:
+                return (" +---+\n" +
+                        " O   |\n" +
+                        "-|-  |\n" +
+                        " |   |\n" +
+                        "/   ===");
+            case 7:
+                return (" +---+\n" +
+                        " O   |\n" +
+                        "-|-  |\n" +
+                        " |   |\n" +
+                        "/ \\ ===");
+        }
+        return "error";
+    }
+}
+
+
